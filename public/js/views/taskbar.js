22b7edb17439a0aff882cd038909706d7fc60fb0
rkulla <rkulla@albatross.(none)>
2013-01-22 00:05:03 -0800

    Moved the rest of the taskbar stuff to a view and added html5 audio chime instead of popup

--- public/js/views/taskbar.js
+++ public/js/views/taskbar.js
@@ -11,10 +11,213 @@ var app = app || {};
            'click #next-task-name a': 'editTask',
            'click #stop-countdown': 'stopCountdown',
            'click #cancel-countdown': 'cancelCountdown',
+           'submit form#taskbar': 'startTimer',
+        },
+
+        initialize: function() {
+            app.hasInitialTask = false;
+            app.stop = false;
+            app.cancel = false;
+            app.timeOnTask = 0;
         },
 
         render: function() {
             return this;
+        },
+
+        startTimer: function(e) {
+            e.preventDefault();
+
+            // Calculate total seconds:
+            var seconds_in_hours = document.getElementById('hours').value * 3600;
+            var seconds_in_minutes = document.getElementById('minutes').value * 60;
+            var seconds = document.getElementById('seconds').value;
+            var total_seconds = (+seconds_in_hours + +seconds_in_minutes + +seconds);
+            if (!total_seconds && !app.hasInitialTask) {
+                total_seconds = 1;
+            }
+
+            var task = document.getElementById('task');
+            var next_task_val = task.value;
+            var next_task = document.getElementById('next-task');
+
+            if (task.value == '') {
+                alert('Please enter a task.');
+                document.taskbar.task.focus();
+                return false;
+            }
+
+            // Validate user input
+            if (total_seconds == 0 && app.hasInitialTask) {
+                alert('Please enter a time.');
+                document.taskbar.hours.select();
+                return false;
+            }
+
+            // Clear last inputted task value
+            task.value = '';
+
+            app.timeOnTask = this.secondsToTime(total_seconds);
+
+            // Set the text for what the next task will be. '\u2014' is 
+            // unicode for &mdash;
+            next_task.innerHTML = next_task_val + ' \u2014 Counting down from: ' + 
+                this.secondsToTime(total_seconds);
+
+            if (app.hasInitialTask) {
+                setTimeout(timerLoop, 1000);
+            } else { 
+                this.alarm();
+                document.getElementById('current-task').style.display = 'block';
+                document.getElementById('current-notes-input').style.display = 'block';
+                document.getElementById('next-notes-input').style.display = 'block';
+            }
+
+            var i = 0;
+            var that = this;
+            function timerLoop() {
+                if (i < total_seconds) {
+                    ++i;
+                    document.getElementById('task-bar').style.display = 'none';
+                    document.getElementById('usage-timer').style.display = 'none';
+                    document.getElementById('next-task-name').style.display = 'block';
+                    document.getElementById('time-bar').style.display = 'block';
+                    var update_time = document.getElementById('update-time');
+                    update_time.innerHTML = that.secondsToTime(total_seconds - i);
+                    setTimeout(timerLoop, 1000);
+                    // Cancel the timer/next task:
+                    if (app.cancel) {
+                        document.getElementById('time-bar').style.display = 'none';
+                        total_seconds = 0;
+                        app.cancel = false;
+                    }
+                    // Stop the timer early:
+                    if (app.stop) {
+                        document.getElementById('time-bar').style.display = 'none';
+                        // Recalulate how much time was spent on the task:
+                        app.timeOnTask = that.secondsToTime(i);
+                        i = total_seconds;
+                    }
+                    that.checkTimer(i, total_seconds);
+                } else {
+                    document.getElementById('task-bar').style.display = 'block';
+                    document.getElementById('usage-timer').style.display = 'none';
+                    document.getElementById('next-task-name').style.display = 'none';
+                    document.taskbar.task.focus();
+                }
+            }
+        },
+
+        checkTimer: function(i, total_seconds) {
+            if (i == total_seconds) {
+                this.alarm();
+            }
+        },
+
+        alarm: function() {
+            var next_task = document.getElementById('next-task');
+            var current_task_text = document.getElementById('current-task-text');
+            var completed_tasks = document.getElementById('completed-tasks');
+            var question_mark = document.getElementById('question-mark');
+            var next_task_val = next_task.innerHTML.substring(0, 
+                next_task.innerHTML.indexOf('\u2014'));
+
+            // Alert the user that the task is ready to begin
+            if (app.hasInitialTask && !app.stop) {
+                document.getElementById('chime').play();
+            }
+
+            app.stop = false; // reset
+
+            // Set hasInitialTask to true
+            app.hasInitialTask = true;
+
+            // Delete '?' for 'completed_tasks' when it receives an actual value.
+            if (current_task_text.innerHTML != '?' && next_task.innerHTML != '?' &&
+                    question_mark.innerHTML == '?') {
+                question_mark.innerHTML = '';
+            }
+
+            // Apppend the current task to the Completed Tasks box:
+            if (current_task_text.innerHTML != '?') {
+                var s = current_task_text.innerHTML + ' \u2014 Time on task: ' + 
+                        app.timeOnTask;
+                var ul = document.getElementById('completed-tasks');
+                var li = document.createElement('li'); 
+                li.appendChild(document.createTextNode(s + ' Ended: ' + this.getTimeNow()));
+                ul.appendChild(li);
+            }
+
+            // Make the current task the value of Next Task
+            current_task_text.innerHTML = next_task.innerHTML.substring(0, 
+                next_task.innerHTML.indexOf('\u2014'));
+
+            this.updateNotes();
+
+            // Change the task input bar to accept next task(s)
+            if (app.hasInitialTask) {
+                document.getElementById('task-desc').innerHTML = 'Next Task ';
+                document.getElementById('usage-no-timer').style.display = 'none';
+                document.getElementById('usage-timer').style.display = 'block';
+                document.getElementById('timer-input').style.display = 'block';
+            }
+
+            this.cleanUp();
+        },
+
+
+        // Moves the next task's notes to the current task's notes
+        updateNotes: function() {
+            document.getElementById('current-textarea').value = 
+                document.getElementById('next-textarea').value;
+
+            document.getElementById('next-textarea').value = '';
+        },
+
+        cleanUp: function() {
+            document.getElementById('next-task').innerHTML = '?';
+            // Make the countdown clock disappear:
+            document.getElementById('time-bar').style.display = 'none';
+            document.taskbar.task.focus();
+        },
+
+        secondsToTime: function(total_seconds) {
+            var hours = Math.floor(total_seconds / 3600);
+            var minutes = Math.floor((total_seconds % 3600) / 60);
+            var seconds = Math.round((((total_seconds % 3600) / 60) - minutes) * 60);
+
+            // Format the text and include proper pluralization
+            var hours_text = (hours == 1 ? ' hour, ' : ' hours, ');
+            var minutes_text = (minutes == 1 ? ' minute and ' : ' minutes and ');
+            var seconds_text = (seconds == 1 ? ' second.' : ' seconds.');
+
+            return hours + hours_text + minutes + minutes_text + seconds + seconds_text;
+        },
+
+        getTimeNow: function() {
+            // Figure out what the time/date is right now
+            // Uses the format: month/day/year hour:minute:second AM/PM
+            var currentTime = new Date();
+            var month = currentTime.getMonth() + 1;
+            var day = currentTime.getDate();
+            var year = currentTime.getFullYear();
+            var hour = currentTime.getHours();
+            var minute = currentTime.getMinutes();
+
+            if (minute < 10) {
+                minute = '0' + minute;
+            }
+            var second = currentTime.getSeconds();
+            if (second < 10) {
+                second = '0' + second;
+            }
+            var ampm = hour > 11 ? 'PM' : 'AM';
+
+            // Convert from 24 hour time to 12 hour time:
+            hour = hour % 12 || 12;
+
+            return month + '/' + day + '/' + year + ' ' + hour + ':' + 
+                        minute + ':' + second + ' ' + ampm;
         },
 
         editTask: function(e) {
@@ -26,7 +229,7 @@ var app = app || {};
             if (current.indexOf('\u2014') != -1) { // Edit next task
                 var new_task = prompt(msg, current.substring(0, current.indexOf('\u2014')));
                 if (new_task != null && new_task != '') {
-                    new_task += ' \u2014 Counting down from: ' + unitaskrObj.timeOnTask;
+                    new_task += ' \u2014 Counting down from: ' + app.timeOnTask;
                 }
             } else {
                 var new_task = prompt(msg, current); // Edit current task
@@ -46,12 +249,12 @@ var app = app || {};
 
         stopCountdown: function(e) {
             e.preventDefault();
-            unitaskrObj.stop = 1;
+            app.stop = true;
         },
 
         cancelCountdown: function(e) {
             e.preventDefault();
-            unitaskrObj.cancel = 1;
+            app.cancel = true;
         },
 
     });
