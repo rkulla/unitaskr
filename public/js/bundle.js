1adba83aadc17ec2d7bc899abb28c74a14aee7ce
Ryan Kulla <rkulla@gmail.com>
2014-05-24 18:06:43 -0700

    Adds drag and drop functionality via native html5

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -17957,28 +17957,41 @@ module.exports = Backbone.View.extend({
 },{"../collections/todos":11,"backbone":5,"jquery":7}],21:[function(require,module,exports){
 'use strict';
 
-var $ = require('jquery');
-var Backbone = require('backbone');
-var _ = require('underscore');
+var $ = require('jquery'),
+    Backbone = require('backbone'),
+    _ = require('underscore'),
+    $taskbarTask,
+    dragSrcEl = null;
 
 module.exports = Backbone.View.extend({
 
     tagName: 'li',
 
+    attributes: function () {
+        return { 'draggable' : 'true' }
+    },
+
     events: {
         'click .deleteTodoTask': 'deleteTodoTask',
         'click .nextTodoTask': 'setNextTodoTask',
+        'dragstart': 'handleDragStart',
+        'dragenter': 'handleDragEnter',
+        'dragleave': 'handleDragLeave',
+        'dragover': 'handleDragOver',
+        'dragend': 'handleDragEnd',
+        'drop': 'handleDrop',
     },
 
     initialize: function() {
-        this.$task = $('#todo-task').val();
+        $taskbarTask = $('#taskbar #task');
     },
 
     render: function() {
         $('#todo-task').val('');
-        var $todoTaskTemplate = _.template($('#todoTaskTemplate').html());
+        var $todoTaskTemplate = _.template($('#todoTaskTemplate').html()),
+            modeltask = this.model.get('task');
+
         this.$el.append($todoTaskTemplate);
-        var modeltask = this.model.get('task');
         this.$el.append(modeltask);
         this.model.save(modeltask); // save a copy to localStorage
         return this;
@@ -17993,10 +18006,51 @@ module.exports = Backbone.View.extend({
     setNextTodoTask: function(e) {
         e.preventDefault();
         var modeltask = this.model.get('task');
-        $('#taskbar #task').val(modeltask);
-        $('#taskbar #task').focus();
+        $taskbarTask.val(modeltask);
+        $taskbarTask.focus();
     },
 
+    handleDragStart: function(e) {
+        this.$el.css('opacity','0.4'); 
+        dragSrcEl = this.$el;
+        // e is jQuery's event object, e.originalEvent is the one we want
+        e.originalEvent.dataTransfer.effectAllowed = 'move'; 
+        // Set the drop payload
+        e.originalEvent.dataTransfer.setData('text/plain', this.$el.html());
+    },
+
+    handleDragEnter: function(e) {
+        this.$el.addClass('over');
+    },
+
+    handleDragLeave: function(e) {
+        this.$el.removeClass('over');
+    },
+
+    handleDragOver: function(e) {
+        e.preventDefault(); // Important: Allows us to drop.
+        e.originalEvent.dataTransfer.dropEffect = 'move';
+    },
+
+    handleDragEnd: function(e) {
+        this.$el.css('opacity', '1.0');
+
+        $('li.over').each(function(index) {
+            $(this).removeClass('over');
+        })
+    },
+    
+    handleDrop: function(e) {
+        e.stopPropagation();
+
+        // Don't do anything if dropping the same column we're dragging
+        if (dragSrcEl != this.$el) {
+            // swap the elements
+            dragSrcEl.html(this.$el.html());
+            this.$el.html(e.originalEvent.dataTransfer.getData('text/plain'));
+        }
+    },
+    
 });
 
 },{"backbone":5,"jquery":7,"underscore":9}]},{},[10])
